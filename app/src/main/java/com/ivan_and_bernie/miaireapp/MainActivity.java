6369aff2c48230ab66c19d0ed977b80c4e855a12
BernardGU <bernardoc.gutierrezc@gmail.com>
2018-04-10 18:35:59 -0500

    Added the score and message as TextViews, added the buttons to go left and right. (They crash the app)

--- app/src/main/java/com/ivan_and_bernie/miaireapp/MainActivity.java
+++ app/src/main/java/com/ivan_and_bernie/miaireapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ivan_and_bernie.miaireapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLeft() {
+        Intent myIntent = new Intent(MainActivity.this, LeftActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    public void goToRight() {
+        Intent myIntent = new Intent(MainActivity.this, LeftActivity.class);
+        MainActivity.this.startActivity(myIntent);
     }
 }
