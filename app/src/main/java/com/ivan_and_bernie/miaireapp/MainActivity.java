3772fa0f3e333c9c4d3ddb0bf6ed9079444803b7
BernardGU <bernardoc.gutierrezc@gmail.com>
2018-04-10 18:52:55 -0500

    Forgot to add the image cerro_de_la_silla.jpg in the last commit

--- app/src/main/java/com/ivan_and_bernie/miaireapp/MainActivity.java
+++ app/src/main/java/com/ivan_and_bernie/miaireapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.ivan_and_bernie.miaireapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //private ImageButton imgButtonLeft = (ImageButton) findViewById(R.id.setting)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
     public void goToRight() {
         Intent myIntent = new Intent(MainActivity.this, LeftActivity.class);
         MainActivity.this.startActivity(myIntent);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
