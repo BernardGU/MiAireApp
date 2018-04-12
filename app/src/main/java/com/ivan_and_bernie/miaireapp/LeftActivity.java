5e650a5e19df9fa60e929d3a7d261745fdc13aa8
xaaniyaro <xaaniyaro@gmail.com>
2018-04-11 19:51:03 -0500

    Creo que left_activity ya está terminada, olvidé mi cable y no puedo probarla :V

--- app/src/main/java/com/ivan_and_bernie/miaireapp/LeftActivity.java
+++ app/src/main/java/com/ivan_and_bernie/miaireapp/LeftActivity.java
@@ -1,9 +1,14 @@
 package com.ivan_and_bernie.miaireapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LeftActivity extends AppCompatActivity {
+    private ImageButton buttonHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +16,18 @@ public class LeftActivity extends AppCompatActivity {
         //Slide the activity in from the left
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         setContentView(R.layout.activity_left);
+
+        buttonHome = findViewById(R.id.HomeButton);
+        buttonHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                OpenHome();
+            }
+        });
+    }
+
+    public void OpenHome(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
